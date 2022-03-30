8e88fe7e1e027dbd2bd9c6880b826f0b9334f3fb
wolkyura <yura02505@gmail.com>
2022-03-30 21:23:35 +0200

    feat: add button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom';
+import cx from 'classnames';
+
+import styles from './Button.module.scss';
+
+export const Button = (props) => {
+  const classNames = cx(styles.button, { 
+    [styles[props.width]]: props.width,
+    [styles[props.theme]]: props.theme,
+    [styles[props.size]]: props.size,
+  });
+
+  const content = () => (
+    <>
+      {props.icon && <props.icon />}
+      {props.children && <span className={styles.text}>{props.children}</span>}
+    </>
+  );
+
+  if (props.to) {
+    return (
+      <Link to={props.to} target={props.target} className={classNames}>
+        {content()}
+      </Link>
+    );
+  }
+
+  return (
+    <button
+      type={props.type || 'button'}
+      className={classNames}
+      onClick={props.onClick}
+    >
+      {content()}
+    </button>
+  );
+};
