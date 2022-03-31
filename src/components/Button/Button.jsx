4c6091c7168e22da75b05a3a99d3b0bf088991c3
wolkyura <yura02505@gmail.com>
2022-03-31 20:54:08 +0200

    feat: add loading state for button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import cx from 'classnames';
 
+import { ReactComponent as SpinnerIcon } from '../../assets/icons/spinner.svg';
+
 import styles from './Button.module.scss';
 
 export const Button = (props) => {
@@ -8,11 +10,12 @@ export const Button = (props) => {
     [styles[props.width]]: props.width,
     [styles[props.theme]]: props.theme,
     [styles[props.size]]: props.size,
+    [styles.loading]: props.loading,
   });
 
   const content = () => (
     <>
-      {props.icon && <props.icon />}
+      {props.loading ? <SpinnerIcon /> : (props.icon && <props.icon />)}
       {props.children && <span className={styles.text}>{props.children}</span>}
     </>
   );
@@ -28,6 +31,7 @@ export const Button = (props) => {
   return (
     <button
       type={props.type || 'button'}
+      disabled={props.loading}
       className={classNames}
       onClick={props.onClick}
     >
