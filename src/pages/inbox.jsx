347986cdc265844ada302a34e7c8257bc8787465
wolkyura <yura02505@gmail.com>
2022-04-09 20:26:54 +0200

    feat: update inbox page

--- src/pages/inbox.jsx
+++ src/pages/inbox.jsx
@@ -1,82 +1,81 @@
 import React from 'react';
-import axios from 'axios';
 
 import { Button } from '../components/Button';
 import { Container } from '../components/Container';
 import { Text } from '../components/Text';
 import { Grid, GridEl } from '../components/Grid';
+import { Tabs, Tab, TabsItems, TabsItem } from '../components/Tabs';
+import { OrdersMap } from '../components/OrdersMap';
+import { OrdersList } from '../components/OrdersList';
+import { Input } from '../components/Input';
 import { useAuth } from '../hooks/useAuth';
+import { ReactComponent as SearchIcon } from '../assets/icons/search.svg';
+import { ReactComponent as ListIcon } from '../assets/icons/list.svg';
+import { ReactComponent as MapMarkerIcon } from '../assets/icons/map-marker.svg';
+import { ReactComponent as CalendarIcon } from '../assets/icons/calendar.svg';
+import { ReactComponent as TimelineIcon } from '../assets/icons/timeline.svg';
 
 export const Inbox = () => {
-  const [employees, setEmployees] = React.useState(null);
-  const [isError, setIsError] = React.useState(false);
-  const { onLogout, user, isManager } = useAuth();
+  const [activeTab, setActiveTab] = React.useState(0);
+  const { user, isManager } = useAuth();
 
-  // Temporary example of communication with API
-  const getEmployees = () => {
-    axios.get(`http://127.0.0.1:8000/api/employees`)
-      .then(({ data }) => {
-        setEmployees(data || []);
-      })
-      .catch(() => {
-        setIsError(true);
-      });
-  };
+  const orders = [{ id: '001' }, { id: '002' }, { id: '003' }, { id: '004' }, { id: '005' }, { id: '006' }];
+
+  if (isManager) {
+    return (
+      <Container {...(activeTab === 1 && { width: 'full' })}>
+        <Grid>
+          <GridEl size="12">
+            <Container withoutPaddings>
+              <Grid alignItems="flex-end" justifyContent="space-between">
+                <GridEl size={{ xs: 12, md: 5, lg: 3 }}>
+                  <Input 
+                    icon={SearchIcon}
+                    placeholder="Search"
+                  />
+                </GridEl>
+                <GridEl size={{ xs: 12, md: 'auto', lg: 9 }}>
+                  <Tabs active={activeTab} onChange={setActiveTab}>
+                    <Tab id={0} icon={ListIcon}>List</Tab>
+                    <Tab id={1} icon={MapMarkerIcon}>Map</Tab>
+                    <Tab id={2} icon={CalendarIcon}>Calendar</Tab>
+                    <Tab id={3} icon={TimelineIcon}>Timeline</Tab>
+                  </Tabs>
+                </GridEl>
+              </Grid>
+            </Container>
+          </GridEl>
+          <GridEl />
+        </Grid>
+        <TabsItems active={activeTab}>
+          <TabsItem for={0}>
+            <OrdersList orders={orders} />
+          </TabsItem>
+          <TabsItem for={1}>
+            <OrdersMap orders={orders} />
+          </TabsItem>
+          <TabsItem for={2}>
+            Calendar
+          </TabsItem>
+          <TabsItem for={3}>
+            Timeline
+          </TabsItem>
+        </TabsItems>
+      </Container>
+    );
+  }
 
   return (
     <Container>
       <Grid>
         <GridEl size="12">
-          <Text size="h2">Hi {user.name} {user.surname}! ({isManager ? 'manager' : 'employee'})</Text>
+          <Text size="h2">Hi {user.name} {user.surname}! (employee)</Text>
         </GridEl>
         <GridEl size="12">
           <Button to="/ui">
             UI components
           </Button>
         </GridEl>
-        <GridEl size="12">
-          <Button onClick={onLogout}>
-            Log out
-          </Button>
-        </GridEl>
-        {/* Temporary example of communication with API */}
-        {isManager && (
-          <GridEl size="12">
-            <Grid>
-              {!employees && (
-                <GridEl size="12">
-                  <Button onClick={getEmployees}>
-                    Get employees
-                  </Button>
-                </GridEl>
-              )}
-              {employees && !employees.length && (
-                <GridEl size="12">
-                  No employees in your local database
-                </GridEl>
-              )}
-              {employees && (
-                <GridEl size="12">
-                  <Grid>
-                    {employees.map(({ firstName, lastName, age, optional }) => (
-                      <GridEl size="12">
-                        <Text size="h3">{firstName} {lastName}</Text>
-                        <Text>{age} y.o.</Text>
-                        <Text>Info: {optional}</Text>
-                      </GridEl>
-                    ))}
-                  </Grid>
-                </GridEl>
-              )}
-              {isError && (
-                <GridEl size="12">
-                  Check if local API is running on http://127.0.0.1:8000/api<br />
-                  And also check if API_URL=http://127.0.0.1:8000/api is not missing in your .env.local file
-                </GridEl>
-              )}
-            </Grid>
-          </GridEl>
-        )}
       </Grid>
     </Container>
   );
