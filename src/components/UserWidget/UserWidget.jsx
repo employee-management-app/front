90352872b9aeab4479d029540f80b062c0d083ee
wolkyura <yura02505@gmail.com>
2022-04-09 20:42:05 +0200

    feat: add log out option in user widget

--- src/components/UserWidget/UserWidget.jsx
+++ src/components/UserWidget/UserWidget.jsx
@@ -1,13 +1,28 @@
+import React from 'react';
+
 import { useAuth } from '../../hooks/useAuth';
 
 import styles from './UserWidget.module.scss';
 
 export const UserWidget = () => {
-  const { user } = useAuth();
+  const { user, onLogout } = useAuth();
 
   return (
-    <button type="button" className={styles.user}>
-      {user.name[0]}{user.surname[0]}
-    </button>
-  )
+    <span className={styles.widget}>
+      <button type="button" className={styles.user}>
+        {user.name[0]}{user.surname[0]}
+      </button>
+      <div className={styles.dropdown}>
+        <div className={styles.dropdownInner}>
+          <button 
+            type="button" 
+            className={styles.dropdownItem}
+            onClick={onLogout}
+          >
+            Log out
+          </button>
+        </div>
+      </div>
+    </span>
+  );
 };
