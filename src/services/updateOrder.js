7f17328e52624a1eb038da2644779b1e280cf419
wolkyura <yura02505@gmail.com>
2022-05-22 10:47:47 +0200

    fix: schedule form

--- src/services/updateOrder.js
+++ src/services/updateOrder.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 import { sanitazeOrder } from './helpers/sanitazeOrder';
 
 export const updateOrder = (order) => new Promise((resolve, reject) => {
+  console.log(order);
   axios.post(`${process.env.REACT_APP_API_URL}/order-update/${order.id}`, {
     order: order.id,
-    assign: typeof order.assigned === 'object' ? order.assigned.id : order.assigned,
+    assign: (order.assigned && typeof order.assigned === 'object') ? order.assigned.id : order.assigned,
     status: order.priority,
     start_date: order.date.toISOString(),
     measurement_date: order.schedule ? new Date(order.schedule).toISOString() : order.schedule,
