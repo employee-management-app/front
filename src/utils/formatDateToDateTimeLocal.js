aa968d768d069dee388fb4258e7642050eaeb84c
wolkyura <yura02505@gmail.com>
2022-06-29 10:51:33 +0200

    fix: format helper

--- src/utils/formatDateToDateTimeLocal.js
+++ src/utils/formatDateToDateTimeLocal.js
@@ -1,9 +1,9 @@
-import { formatISO9075 } from 'date-fns';
+import { format } from 'date-fns';
 
 export const formatDateToDateTimeLocal = (date) => {
   if (!date) {
     return null;
   }
 
-  return formatISO9075(new Date(date)).slice(0, -3).replace(' ', 'T');
+  return format(new Date(date), `yyyy-MM-dd'T'HH:mm:ss`);
 };
