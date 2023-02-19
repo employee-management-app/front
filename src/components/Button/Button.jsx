3fcffd99530247f42a4ca17c3f76fcb16549d913
wolkyura <yura02505@gmail.com>
2023-02-19 14:59:01 +0100

    refactor: button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -6,23 +6,23 @@ import { ReactComponent as SpinnerIcon } from '../../assets/icons/spinner.svg';
 
 import styles from './Button.module.scss';
 
-export const Button = ({ type = 'button', ...props }) => {
+export const Button = ({ type = 'button', loading = false, disabled = false, ...props }) => {
   const classNames = cx(styles.button, {
     [styles[props.width]]: props.width,
     [styles[props.theme]]: props.theme,
     [styles[props.size]]: props.size,
-    [styles.loading]: props.loading,
-    [styles.disabled]: props.disabled,
+    [styles.loading]: loading,
+    [styles.disabled]: disabled,
   });
 
   const content = () => (
     <>
-      {props.loading ? <SpinnerIcon /> : (props.icon && <props.icon />)}
+      {loading ? <SpinnerIcon /> : (props.icon && <props.icon />)}
       {props.children && <span className={styles.text}>{props.children}</span>}
     </>
   );
 
-  if (props.href && !props.disabled) {
+  if (props.href && !disabled) {
     return (
       <a href={props.href} target={props.target} className={classNames} onClick={props.onClick}>
         {content()}
@@ -30,7 +30,7 @@ export const Button = ({ type = 'button', ...props }) => {
     );
   }
 
-  if (props.to && !props.disabled) {
+  if (props.to && !disabled) {
     return (
       <Link to={props.to} target={props.target} className={classNames} onClick={props.onClick}>
         {content()}
@@ -42,7 +42,7 @@ export const Button = ({ type = 'button', ...props }) => {
     <button
       // eslint-disable-next-line react/button-has-type
       type={type}
-      disabled={props.loading || props.disabled}
+      disabled={loading || disabled}
       className={classNames}
       onClick={props.onClick}
     >
