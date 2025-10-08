7a71db689c15ec9bf5929ce9df3782f2a0d2b204
wolkyura <yura02505@gmail.com>
2025-10-08 20:06:36 +0200

    Remove service worker

--- src/index.jsx
+++ src/index.jsx
@@ -4,44 +4,13 @@ import { Provider } from 'react-redux';
 
 import App from './App';
 import { store } from './store';
-import { useNotification } from './hooks/useNotification';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './styles/global.scss';
-
-const ServiceWorkerWrapper = ({ children }) => {
-  const { pushNotification } = useNotification();
-
-  const reloadPage = (registration) => {
-    registration.waiting.postMessage({ type: 'SKIP_WAITING' });
-    window.location.reload();
-  };
-
-  const onServiceWorkerUpdate = (registration) => {
-    pushNotification({
-      theme: 'info',
-      content: 'New version is available!',
-      action: {
-        label: 'Get the new version',
-        onClick: () => reloadPage(registration),
-      },
-      manualClose: true,
-    });
-  };
-
-  React.useEffect(() => {
-    serviceWorkerRegistration.register({ onUpdate: onServiceWorkerUpdate });
-  }, []);
-
-  return children;
-};
 
 const root = document.getElementById('root');
 
 render(
   <Provider store={store}>
-    <ServiceWorkerWrapper>
-      <App />
-    </ServiceWorkerWrapper>
+    <App />
   </Provider>,
   root,
 );
